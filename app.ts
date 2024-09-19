fa7e9d7990197dac9c9d89c0d3d1dc5298cd6333
alanrochagomes <alanrochaarg2001@gmail.com>
2024-09-19 17:04:51 -0300

    add: pasta class

--- app.ts
+++ app.ts
@@ -1,41 +1,10 @@
-abstract class Account {
-  name: string
-  accountNumber: number
-  balance: number = 0
-
-  constructor(name: string, accountNumber: number) {
-    this.name = name
-    this.accountNumber = accountNumber
-  }
-
-  deposit = () => {
-    console.log('Voce depositou')
-  }
-
-  withdraw = () => {
-    console.log('voce sacou')
-  }
-
-  getBalance= () => {
-    console.log(this.balance)
-  }
-}
-
-class PeopleAccount extends Account {
-  doc_id: number
-
-  constructor(doc_id: number, name: string, accountNumber: number) {
-    super(name, accountNumber)
-    this.doc_id = doc_id
-  }
-}
+import { CompanyAccount } from './class/companyaccount';
+import { PeopleAccount } from './class/PeopleAccount';
 
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Alan', 10)
-console.log(peopleAccount)
+peopleAccount.deposit()
 
-// const adminAccount: Account = new Account('Alan', 1)
-// console.log(adminAccount)
+const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
+companyAccount.deposit()
 
-// const account: Account = new Account('user', 2)
-// console.log(account)
---
+++ class/CompanyAccount.ts
@@ -0,0 +1,16 @@
+import { DioAccount } from "./DioAccount"
+
+export class CompanyAccount extends DioAccount {
+
+  constructor(name: string, accountNumber: number) {
+    super(name, accountNumber)
+  }
+
+  getLoan = (): void => {
+    console.log('voce pegou um emprestimo')
+  }
+
+  deposit = (): void => {
+    console.log('A empresa depositou')
+  }
+}
