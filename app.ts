f60d47e9674180248a621ca138d5df3bd540ecdb
alanrochagomes <alanrochaarg2001@gmail.com>
2024-09-20 08:28:25 -0300

    add: readonly

--- app.ts
+++ app.ts
@@ -3,11 +3,9 @@ import { PeopleAccount } from './class/PeopleAccount';
 
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Alan', 10)
-// peopleAccount.deposit()
 console.log(peopleAccount)
 peopleAccount.deposit()
 
-
-
-// const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-// console.log(companyAccount.deposit())
+const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
+companyAccount.deposit()
+console.log(companyAccount)
